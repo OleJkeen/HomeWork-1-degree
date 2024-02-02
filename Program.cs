cf414f2fecabe06d2b76c336c66bcc854f704d19
OleJkeen <chikenfresh@gmail.com>
2024-02-02 13:46:39 -0500

    first

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Work_1_degree_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomMinNumber = 1;
+            int randomMaxNumber = 100;
+            int randomNumber = random.Next(randomMinNumber, randomMaxNumber);
+            int minDegree = 0;
+            int resultDegree = 1;
+            int baseDegree = 2;
+            
+            while (resultDegree <= randomNumber)
+            {
+                resultDegree *= baseDegree;
+                minDegree++;
+            }
+            
+            Console.WriteLine("Число: " + randomNumber);
+            Console.WriteLine("Минимальная степень двойки, превышающая это число: " + minDegree);
+            Console.WriteLine($"{baseDegree} в степени {minDegree}  равно {resultDegree}");
+            Console.ReadLine();
+        }
+    }
+}
